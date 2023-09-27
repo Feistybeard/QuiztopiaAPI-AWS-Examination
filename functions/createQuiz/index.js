b1f463ce8b922d85e55b4478aeaa267930cb2c1c
Marvan Al Kufai <alkufai@gmail.com>
2023-09-27 23:46:35 +0200

    feat: create a quiz feat: token validation

---
+++ functions/createQuiz/index.js
@@ -0,0 +1,39 @@
+const { sendResponse, sendError } = require('../../responses');
+const { db } = require('../../services/db');
+const { v4: uuidv4 } = require('uuid');
+const middy = require('@middy/core');
+const { validateToken } = require('../../middleware/auth');
+
+const handler = middy()
+  .handler(async (event) => {
+    try {
+      if (!event?.userId || (event?.error && event?.error === '401'))
+        return sendError(401, { success: false, message: 'Invalid token' });
+
+      const quizId = uuidv4();
+      const { quizName } = JSON.parse(event.body);
+      if (!quizName)
+        return sendError(400, { success: false, message: 'Missing quizName' });
+
+      const quiz = {
+        quizId,
+        quizName,
+        questions: [],
+        userId: event.userId,
+      };
+
+      await db
+        .put({
+          TableName: 'QuiztopiaQuizzes',
+          Item: quiz,
+        })
+        .promise();
+
+      return sendResponse(200, { success: true, quizId: quizId });
+    } catch (error) {
+      return sendError(500, error);
+    }
+  })
+  .use(validateToken);
+
+module.exports = { handler };
---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const validateToken = {
+  before: async (request) => {
+    try {
+      const token = request.event.headers.authorization.replace('Bearer ', '');
+
+      if (!token) throw new Error();
+
+      const data = jwt.verify(token, 'secretpassword');
+      console.log('data', data);
+      request.event.userId = data.userId;
+      request.event.userName = data.userName;
+
+      return request.response;
+    } catch (error) {
+      request.event.error = '401';
+      return request.response;
+    }
+  },
+  onError: async (request) => {
+    request.event.error = '401';
+    return request.response;
+  },
+};
+
+module.exports = { validateToken };
