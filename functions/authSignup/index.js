b5508fa721b6bfa6dd29133ac568b1091b477060
Marvan Al Kufai <alkufai@gmail.com>
2023-09-27 16:47:43 +0200

    feat: user signup

---
+++ functions/authSignup/index.js
@@ -0,0 +1,54 @@
+const { sendResponse, sendError } = require('../../responses');
+const { db } = require('../../services/db');
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
+
+exports.handler = async (event, context) => {
+  try {
+    const { userName, password } = JSON.parse(event.body);
+
+    if (!userName || !password) {
+      return sendResponse(400, {
+        success: false,
+        message: 'Missing username or password',
+      });
+    }
+
+    const userToCreate = {
+      userId: uuidv4(),
+      userName,
+      password,
+    };
+
+    userToCreate.password = await bcrypt.hash(userToCreate.password, 10);
+
+    const checkUserExists = await db
+      .scan({
+        TableName: 'QuiztopiaUsers',
+        FilterExpression: 'userName = :userName',
+        ExpressionAttributeValues: {
+          ':userName': userToCreate.userName,
+        },
+      })
+      .promise();
+
+    if (checkUserExists.Items.length > 0) {
+      return sendResponse(400, {
+        success: false,
+        message: 'User already exists',
+      });
+    } else {
+      await db
+        .put({
+          TableName: 'QuiztopiaUsers',
+          Item: userToCreate,
+        })
+        .promise();
+
+      return sendResponse(200, { success: true });
+    }
+  } catch (error) {
+    console.log(error);
+    return sendError(500, error);
+  }
+};
